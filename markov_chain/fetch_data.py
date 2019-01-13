fa0aae39f59cbacbdfb192ee926aa373d592642b
Christopher Girard <momentum.christopher@gmail.com>
2019-01-13 15:10:16 -0500

    Ingredient list fully fetched.
    An ingredient or two has parantheses precision, something to check during polishing at the very end of the project.
    
    Removed pseudo_code comment
    
    Turned all debugging prints into comments

--- markov_chain/fetch_data.py
+++ markov_chain/fetch_data.py
@@ -17,23 +17,14 @@ with open("Ingredients HTML.html", "w") as ing_html:
 
 for each in soup.find_all("a", string=True):
   if each.parent.name == "li":
-    print(each.get_text())
+    ingr_list.append(each.get_text())
+    #print ("Added %s") % each.get_text()
+
   if each.get_text() == "Zucchini":
   	break
 
-
-
-"""
-"Imports the HTML code into a variable.
-
-"For ingredients, the important line is this one:
-<li><a href="/wiki/Cookbook:Maple_Syrup" title="Cookbook:Maple Syrup">Maple syrup</a></li>
-
-"<li></li> is a list
-
-"<a href="" title=""></a> is a link
-
-"What we need is between both: <li><a href"" title="">MapleSyrup</a></li>
-"""
-
-
+# print
+# print ("Ingredient list:")
+# print
+# for each in ingr_list:
+# 	print each + (","),
