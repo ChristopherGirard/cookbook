6f34d0b40332d4990ef87ec1227859fe9ceea283
Christopher Girard <momentum.christopher@gmail.com>
2019-01-12 22:35:09 -0500

    Added Ingredients HTML code into a .txt for better readability. Added comment explaining what we're looking for in the code and what we'll be splitting. Renamed text.txt to Ingredients_HTML.txt

--- markov_chain/fetch_data.py
+++ markov_chain/fetch_data.py
@@ -3,5 +3,15 @@ import urllib2
 
 response = urllib2.urlopen("https://en.wikibooks.org/wiki/Cookbook:Ingredients")
 ingredients_html = response.read()
+"""
+"Imports the HTML code into a variable.
 
-print ingredients_html
+"For ingredients, the important line is this one:
+<li><a href="/wiki/Cookbook:Maple_Syrup" title="Cookbook:Maple Syrup">Maple syrup</a></li>
+
+"<li></li> is a list
+
+"<a href="" title=""></a> is a link
+
+"What we need is between both: <li><a href"" title="">MapleSyrup</a></li>
+"""
